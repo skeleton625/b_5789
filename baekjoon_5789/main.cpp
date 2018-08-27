63431bb41a697389d42963602fc7949886a7fd6b
WaniFi <skelpd24@gmail.com>
2018-08-27 18:54:55 +0900

    5789 문제 개선

--- baekjoon_5789/main.cpp
+++ baekjoon_5789/main.cpp
@@ -12,30 +12,8 @@ int main() {
 			if (n[i] == n[strlen(n) - i - 1]) c = true;
 			else c = false;
 		}
-		if (c) printf("Do-it");
-		else printf("Do-it-Not");
+		if (c) printf("Do-it\n");
+		else printf("Do-it-Not\n");
 	}
 	return 0;
 }
-/*
-#include <iostream>
-#include <string.h>
-using namespace std;
-
-bool c;
-int t;
-char n[1001];
-int main() {
-	cin >> t;
-	while (t--) {
-		cin >> n;
-		for (int i = 0; i < strlen(n) / 2; i++) {
-			if (n[i] == n[strlen(n) - i - 1]) c = true;
-			else c = false;
-		}
-		if (c) cout << "Do-it" << endl;
-		else cout << "Do-it-Not" << endl;
-	}
-	return 0;
-}
-*/
